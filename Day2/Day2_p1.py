99149cad95b8d4132d0148ea277c8c2589c2b8e1
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2019-12-09 12:23:47 +0100

    Day 2 part 1 completed

--- Day2/Day2_p1.py
+++ Day2/Day2_p1.py
@@ -1,10 +1,39 @@
 
 try:
-  file = open("day1_input.txt", "r")
+  file = open("Day2_input.txt", "r")
 except IOError:
   print("Error: Can't open file")
   import sys
   sys.exit(0)
 
-program = [1,9,10,3,2,3,11,0,99,30,40,50]
+lines = file.read().split(',')
+file.close
+lines = [int(i) for i in lines]
+
+lines[1] = 12
+lines[2] = 2
+
+def runProgram(program):
+    pos = 0
+    while program[pos] != 99:
+        if (program[pos] == 1):
+            program = op1(program,pos)
+        elif(program[pos] == 2):
+            program = op2(program,pos)
+        pos += 4
+    return program
+
+def op1(list, pos):
+    list[list[pos+3]] = list[list[pos+1]] + list[list[pos+2]]
+    return list
+
+def op2(list, pos):
+    list[list[pos+3]] = list[list[pos+1]] * list[list[pos+2]]
+    return list
+
+print(runProgram(lines))
+
+
+
+
 
